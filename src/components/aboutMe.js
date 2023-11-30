543d85c32cb1a37bbfb5ed80342d8c4786a22036
cotaluis <135561226+cotaluis@users.noreply.github.com>
2023-11-29 21:27:26 -0800

    added src folder

---
+++ src/components/aboutMe.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const AboutMe = () => {
+  return (
+    <section id="about">
+      <h2>About Me</h2>
+      <p>Hey! My names Luis Cota, I am a rising full stack developer who loves tackling challenges and is always determined to find solutions. Hardworking and passionate about learning, I approach coding tasks with enthusiasm and a commitment to doing things well. In the ever-changing world of technology, I stand out as someone who loves pushing boundaries, adapting to new trends, and solving problems with a positive attitude.
+
+
+
+
+
+</p>
+    </section>
+  );
+}
+
+export default AboutMe;
